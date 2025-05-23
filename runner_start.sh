1a4a062a110dbbec944d3ba8b72e2b7ab72e5c3a
Stefan Bosak <stefan.bosak@gmail.com>
2025-05-24 00:54:33 +0200

    Extended Linux GHA workflow example
    Signed-off-by: Stefan Bosak <stefan.bosak@gmail.com>

--- runner_start.sh
+++ runner_start.sh
@@ -67,7 +67,7 @@ for platform in "${PLATFORMS[@]}"; do
                        --env RUNNER_GROUP_NAME="${RUNNER_GROUP_NAME}" \
                        --env RUNNER_LABELS="${RUNNER_LABELS}" \
                        --env WORKSPACE_ROOT_DIR="${WORKSPACE_ROOT_DIR}" \
-                       --network=host --detach --rm --name "${runner_name}" \
+                       --network=host --detach --rm --name "${runner_name}" --group-add "docker" \
                        "${CONTAINER_REPOSITORY}${CONTAINER_IMAGE_NAME}${CONTAINER_IMAGE_TAG}"
 done
 
